fee1fa4957e686fe0d344ed85ed4983f88216f3c
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-10-23 13:10:00 +0300

    task#1 1st commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import SearchForm from './SearchForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import GentreToggle from './GenreToggle';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Hello, world, from React!</h1>
+    <App />
+    <SearchForm />
+    <GentreToggle />
+  </React.StrictMode>
+);
